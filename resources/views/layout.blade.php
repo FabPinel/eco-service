25a865f14ae6150327239b792d74c4b9955fa79d
Fabien Pinel <fabien.pinel08@gmail.com>
2023-12-16 17:32:19 +0100

    Initialisation d'AlpineJs

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @vite('resources/css/app.css')
     <title>Laravel</title>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
 
 <body class="antialiased">
