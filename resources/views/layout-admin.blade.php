121e79c318e45103b24397e1f45ed60d9d4f0a56
Fabien Pinel <fabien.pinel08@gmail.com>
2023-12-18 17:01:03 +0100

    Dashboard : Création des fichiers + route + navbar admin

---
+++ resources/views/layout-admin.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite('resources/css/app.css')
+    <title>Laravel</title>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="antialiased">
+    @include('partials.navbar-admin')
+    <div class="content">
+        @yield('admin-content')
+    </div>
+</body>
+
+</html>
