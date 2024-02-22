c96c62c470c58ba1e6c6e4cd6d52e2b774bac000
Fabien Pinel <fabien.pinel08@gmail.com>
2024-02-22 11:45:24 +0100

    Boutique : Filtre des produits par catégorie en version desktop

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -643,56 +643,56 @@
 
                 <section aria-labelledby="products-heading" class="pb-24 pt-6">
                     <h2 id="products-heading" class="sr-only">Products</h2>
-
-                    <div class="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
+                    
+                    <div class="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4" x-data="{
+                        isOpen: true,
+                        selectedCategory: null,
+                        toggleCategory(categoryId) {
+                            if (this.selectedCategory === categoryId) {
+                                this.selectedCategory = null;
+                            } else {
+                                this.selectedCategory = categoryId;
+                            }
+                        }
+                        }">
                         <!-- Filters -->
-                        {{-- <form class="hidden lg:block"> --}}
-                        <div>
-                            <h3 class="sr-only">Categories</h3>
-
-                            <div x-data="{ isOpen: true }" class="border-b border-gray-200 py-6">
-                                <h3 class="-my-3 flow-root">
-                                    <!-- Expand/collapse section button -->
-                                    <button @click="isOpen = !isOpen"
-                                        class="flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500"
-                                        aria-controls="filter-section-0">
-                                        <span class="font-medium text-gray-900">Nos categories</span>
-                                        <span class="ml-6 flex items-center">
-                                            <!-- Expand icon, show/hide based on section open state. -->
-                                            <svg x-show="!isOpen" class="h-5 w-5" viewBox="0 0 20 20"
-                                                fill="currentColor" aria-hidden="true">
-                                                <path
-                                                    d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
-                                            </svg>
-                                            <!-- Collapse icon, show/hide based on section open state. -->
-                                            <svg x-show="isOpen" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"
-                                                aria-hidden="true">
-                                                <path fill-rule="evenodd"
-                                                    d="M4 10a.75.75 0 01.75-.75h10.5a.75.75 0 010 1.5H4.75A.75.75 0 014 10z"
-                                                    clip-rule="evenodd" />
-                                            </svg>
-                                        </span>
-                                    </button>
-                                </h3>
-                                <!-- Filter section, show/hide based on section state. -->
-                                <div x-show="isOpen" class="pt-6" id="filter-section-0">
-                                    <div class="space-y-4">
-                                        @foreach ($categories as $category)
-                                            <div class="flex items-center">
-                                                <input id="filter-category-0" name="category[]" type="checkbox"
-                                                    class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500">
-                                                <label for="filter-category-0" class="ml-3 text-sm text-gray-600">{{ $category->name }}</label>
-                                            </div>
-                                        @endforeach
-                                    </div>
+                        <div class="border-b border-gray-200 py-6">
+                            <h3 class="-my-3 flow-root">
+                                <!-- Expand/collapse section button -->
+                                <button @click="isOpen = !isOpen" class="flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500" aria-controls="filter-section-0">
+                                    <span class="font-medium text-gray-900">Nos categories</span>
+                                    <span class="ml-6 flex items-center">
+                                        <!-- Expand icon, show/hide based on section open state. -->
+                                        <svg x-show="!isOpen" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                            <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
+                                        </svg>
+                                        <!-- Collapse icon, show/hide based on section open state. -->
+                                        <svg x-show="isOpen" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                            <path fill-rule="evenodd" d="M4 10a.75.75 0 01.75-.75h10.5a.75.75 0 010 1.5H4.75A.75.75 0 014 10z" clip-rule="evenodd" />
+                                        </svg>
+                                    </span>
+                                </button>
+                            </h3>
+                            <!-- Filter section, show/hide based on section state. -->
+                            <div x-show="isOpen" class="pt-6" id="filter-section-0">
+                                <div class="space-y-4">
+                                    @foreach ($categories as $category)
+                                        <div class="flex items-center">
+                                            <input id="filter-category-{{ $category->id }}" name="category[]" type="checkbox"
+                                                class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                                                x-on:click="toggleCategory('{{ $category->id }}')"
+                                                x-bind:checked="selectedCategory === '{{ $category->id }}'">
+                                            <label for="filter-category-{{ $category->id }}" class="ml-3 text-sm text-gray-600">{{ $category->name }}</label>
+                                        </div>
+                                    @endforeach
                                 </div>
                             </div>
                         </div>
-                        {{-- </form> --}}
                         <!-- Product grid -->
                         <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-4 lg:col-span-3 lg:gap-x-8">
                             @foreach ($products as $product)
-                                <a href="{{ route('shop.productName', $product->id) }}" class="group text-sm">
+                                <a href="{{ route('shop.productName', $product->id) }}" class="group text-sm"
+                                    x-show="!selectedCategory || '{{ $product->id_category }}' == selectedCategory">
                                     <div
                                         class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
                                         <img class="h-full w-full object-cover object-center"
@@ -705,7 +705,8 @@
                             @endforeach
                         </div>
                     </div>
-                </section>
+                </section>                
+                
             </main>
         </div>
     </div>
