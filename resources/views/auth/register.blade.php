3694d4968e347e15a3d566bc3e5ab50f7d42c56d
Fabien Pinel <fabien.pinel08@gmail.com>
2024-02-24 09:18:27 +0100

    Navbar: Fix navbar register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,5 @@
-@extends('layout-admin')
-@section('admin-content')
+@extends('layout')
+@section('content')
     <div class="mt-20 mx-auto max-w-4xl flex">
         <div class="w-2/4">
             <img src="https://images.unsplash.com/photo-1582803824122-f25becf36ad8?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
