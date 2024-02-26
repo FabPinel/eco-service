3a54d66ee8558a19c1b6a00c3f5ae7ceeb8183b9
Fabien Pinel <fabien.pinel08@gmail.com>
2024-02-26 13:22:49 +0100

    Login: refont du css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,26 +2,21 @@
 @section('content')
     <section class="bg-gray-50 ">
         <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900">
-                <img class="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo">
-                Flowbite
-            </a>
             <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                     <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-                        Sign in to your account
+                        Connexion à votre compte
                     </h1>
                     <form class="space-y-4 md:space-y-6" action="{{ route('login.authenticate') }}" method="POST">
                         @csrf
                         <div>
-                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Your
-                                email</label>
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Votre email</label>
                             <input type="email" name="email" id="email"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="name@company.com" required="">
                         </div>
                         <div>
-                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Mot de passe</label>
                             <input type="password" name="password" id="password" placeholder="••••••••"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 required="">
@@ -29,26 +24,16 @@
                         <div class="flex items-center justify-between">
                             <div class="flex items-start">
                                 <div class="flex items-center h-5">
-                                    <input id="remember" aria-describedby="remember" type="checkbox"
-                                        class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:ring-offset-gray-800">
-                                </div>
-                                <div class="ml-3 text-sm">
-                                    <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
+                                  
                                 </div>
                             </div>
-                            <a href="#"
-                                class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot
-                                password?</a>
+                            <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Mot de passe oublié ?</a>
                         </div>
                         <button type="submit"
-                            class="text-white bg-gradient-to-r from-purple-500 via-purple-600
-                            to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300
-                            dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg
-                            dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Sign
-                            in</button>
+                            class="text-white bg-[#1c3242] hover:bg-[#374a56] font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Se connecter</button>
                         <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                            Don’t have an account yet? <a href="/register"
-                                class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                            Vous n'avez pas encore de compte ? <a href="/register"
+                                class="font-medium text-[#e88229] hover:underline hover:text-[#EEA25F]">S'inscrire</a>
                         </p>
                     </form>
                 </div>
