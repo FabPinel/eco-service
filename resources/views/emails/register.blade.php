03b34dedfb26fd552ca5ed9a1b4675d92ce3657f
Aniss <aniss.briki@gmail.com>
2024-02-26 14:38:15 +0100

    Email de vérification lors de la création du compte

--- resources/views/emails/register.blade.php
+++ resources/views/emails/register.blade.php
@@ -18,3 +18,224 @@
 </body>
 
 </html>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html dir="ltr" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office"><head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <meta name="x-apple-disable-message-reformatting">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="telephone=no" name="format-detection">
+    <title></title> 
+    <!--[if (mso 16)]>
+      <style type="text/css">
+      a {text-decoration: none;}
+      </style>
+      <![endif]--> 
+    <!--[if gte mso 9]><style>sup { font-size: 100% !important; }</style><![endif]--> 
+    <!--[if gte mso 9]>
+  <xml>
+      <o:OfficeDocumentSettings>
+      <o:AllowPNG></o:AllowPNG>
+      <o:PixelsPerInch>96</o:PixelsPerInch>
+      </o:OfficeDocumentSettings>
+  </xml>
+  <![endif]-->
+   <!--[if mso]>
+   <style type="text/css">
+       ul {
+    margin: 0 !important;
+    }
+    ol {
+    margin: 0 !important;
+    }
+    li {
+    margin-left: 47px !important;
+    }
+  
+   </style><![endif]
+  --></head>
+   <body class="body">
+    <div dir="ltr" class="es-wrapper-color">
+     <!--[if gte mso 9]>
+              <v:background xmlns:v="urn:schemas-microsoft-com:vml" fill="t">
+                  <v:fill type="tile" color="#efefef"></v:fill>
+              </v:background>
+          <![endif]-->
+     <table class="es-wrapper" width="100%" cellspacing="0" cellpadding="0">
+      <tbody>
+       <tr>
+        <td class="esd-email-paddings" valign="top">
+         <table cellpadding="0" cellspacing="0" class="es-content esd-header-popover" align="center">
+          <tbody>
+           <tr>
+            <td class="es-adaptive esd-stripe" align="center">
+             <table class="es-content-body" style="background-color: rgb(239, 239, 239);" width="600" cellspacing="0" cellpadding="0" bgcolor="#ffffff" align="center">
+              <tbody>
+               <tr>
+                <td class="esd-structure es-p15t es-p15b es-p20r es-p20l" align="left">
+                 <!--[if mso]><table width="560" cellpadding="0" cellspacing="0"><tr><td width="270" valign="top"><![endif]-->
+                 <table class="es-left" cellspacing="0" cellpadding="0" align="left">
+                  <tbody>
+                   <tr>
+                    <td class="esd-container-frame" width="270" align="left">
+                     <table width="100%" cellspacing="0" cellpadding="0">
+                      <tbody>
+                       <tr>
+                        <td class="es-infoblock esd-block-text es-m-txt-c" align="left"><p>Put your preheader text here</p></td>
+                       </tr>
+                      </tbody>
+                     </table></td>
+                   </tr>
+                  </tbody>
+                 </table> 
+                 <!--[if mso]></td><td width="20"></td><td width="270" valign="top"><![endif]-->
+                 <table class="es-right" cellspacing="0" cellpadding="0" align="right">
+                  <tbody>
+                   <tr>
+                    <td class="esd-container-frame" width="270" align="left">
+                     <table width="100%" cellspacing="0" cellpadding="0">
+                      <tbody>
+                       <tr>
+                        <td align="right" class="es-infoblock esd-block-text es-m-txt-c"><p><a href="https://viewstripo.email" target="_blank" class="view">View in browser</a></p></td>
+                       </tr>
+                      </tbody>
+                     </table></td>
+                   </tr>
+                  </tbody>
+                 </table> 
+                 <!--[if mso]></td></tr></table><![endif]--></td>
+               </tr>
+              </tbody>
+             </table></td>
+           </tr>
+          </tbody>
+         </table>
+         <table cellpadding="0" cellspacing="0" class="es-header" align="center">
+          <tbody>
+           <tr>
+            <td class="esd-stripe" align="center">
+             <table class="es-header-body" style="background-color: rgb(254, 245, 228);" width="600" cellspacing="0" cellpadding="0" bgcolor="#fef5e4" align="center">
+              <tbody>
+               <tr>
+                <td class="esd-structure es-p5t es-p5b es-p15r es-p15l" align="left" bgcolor="#1c3242" style="background-color:#1c3242">
+                 <!--[if mso]><table width="570" cellpadding="0" cellspacing="0"><tr><td width="180" valign="top"><![endif]-->
+                 <table class="es-left" cellspacing="0" cellpadding="0" align="left">
+                  <tbody>
+                   <tr>
+                    <td width="180" class="esd-container-frame" align="left"><table cellpadding="0" cellspacing="0" width="100%" role="presentation"><tbody><tr>
+              <td align="left" class="esd-block-text">
+                  <p>​</p>
+              </td>
+          </tr></tbody></table></td>
+                   </tr>
+                  </tbody>
+                 </table> 
+                 <!--[if mso]></td><td width="20"></td><td width="370" valign="top"><![endif]-->
+                 <table cellspacing="0" cellpadding="0" align="right">
+                  <tbody>
+                   <tr>
+                        
+                    <td class="esd-container-frame" width="370" align="left">
+                        <table width="100%" cellspacing="0" cellpadding="0">
+                            <tbody><tr>
+                        <td class="esd-block-image es-m-p0l es-p15l es-m-txt-c" align="left" style="font-size:0"><a href="https://viewstripo.email/" target="_blank"><img src="https://efrwlxp.stripocdn.email/content/guids/CABINET_18a75c870884b71ca782936b8685f9926556f0fb81ea8f82fc8118f20da9bfda/images/logo_eco_service.png" alt="Logo EcoService" title="Logo EcoService" width="118" class="img-6697"></a></td>
+                       </tr></tbody></table>
+                    </td>
+                
+                        
+                </tr>
+                  </tbody>
+                 </table> 
+                 <!--[if mso]></td></tr></table><![endif]--></td>
+               </tr>
+              </tbody>
+             </table></td>
+           </tr>
+          </tbody>
+         </table>
+         <table class="es-content" cellspacing="0" cellpadding="0" align="center">
+          <tbody>
+           <tr>
+            <td class="esd-stripe" align="center">
+             <table class="es-content-body" width="600" cellspacing="0" cellpadding="0" bgcolor="#ffffff" align="center">
+              <tbody>
+               <tr>
+                <td class="esd-structure es-p10t es-p10b es-p20r es-p20l" esd-general-paddings-checked="false" align="left">
+                 <table width="100%" cellspacing="0" cellpadding="0">
+                  <tbody>
+                   <tr>
+                    <td class="esd-container-frame" width="560" valign="top" align="center">
+                     <table style="border-radius: 0px; border-collapse: separate;" width="100%" cellspacing="0" cellpadding="0">
+                      <tbody>
+                       <tr>
+                        <td class="esd-block-text es-p10t es-p15b" align="center"><h1 style="font-family:helvetica,&#39;helvetica neue&#39;,arial,verdana,sans-serif"><strong>Confirmez votre adresse mail</strong></h1></td>
+                       </tr>
+                       <tr>
+              <td align="center" class="esd-block-button">
+                  <span class="es-button-border" style="background:#e88229">
+                      <a href="{{ route('user.verify', $token) }}" class="es-button" target="_blank" style="background:#e88229;mso-border-alt:10px solid #e88229;font-weight:bold">Confirmer mon adresse mail</a>
+                  </span>
+              </td>
+          </tr><tr>
+                        <td class="esd-block-text es-p5t es-p5b es-p40r es-p40l" align="center"><h6 style="font-family:helvetica,&#39;helvetica neue&#39;,arial,verdana,sans-serif">​</h6><h6 style="font-family:helvetica,&#39;helvetica neue&#39;,arial,verdana,sans-serif" class=" es-m-txt-c">Cher(e) [Nom du destinataire],</h6><h6 style="font-family:helvetica,&#39;helvetica neue&#39;,arial,verdana,sans-serif" class=" es-m-txt-c">​</h6><h6 style="font-family:helvetica,&#39;helvetica neue&#39;,arial,verdana,sans-serif" class=" es-m-txt-c">Nous sommes ravis de vous confirmer que votre compte sur <strong>Eco-service</strong> a bien été créé avec succès !</h6><h6 style="font-family:helvetica,&#39;helvetica neue&#39;,arial,verdana,sans-serif" class=" es-m-txt-c">​</h6><h6 style="font-family:helvetica,&#39;helvetica neue&#39;,arial,verdana,sans-serif" class=" es-m-txt-c">Afin de finaliser le processus d'inscription et de garantir la sécurité de votre compte, veuillez confirmer votre adresse e-mail en cliquant sur le lien ci-dessus.</h6><h6 style="font-family:helvetica,&#39;helvetica neue&#39;,arial,verdana,sans-serif" class=" es-m-txt-c">​</h6><h6 style="font-family:helvetica,&#39;helvetica neue&#39;,arial,verdana,sans-serif" class=" es-m-txt-c">Une fois votre adresse e-mail confirmée, vous pourrez accéder à toutes les fonctionnalités de notre plateforme et commencer à profiter de nos services.</h6><p class=" es-m-txt-c">​</p><h6 style="font-family:helvetica,&#39;helvetica neue&#39;,arial,verdana,sans-serif" class=" es-m-txt-c">Si vous avez des questions ou avez besoin d'aide, n'hésitez pas à contacter notre équipe d'assistance.</h6><p>​</p><h6 style="font-family:helvetica,&#39;helvetica neue&#39;,arial,verdana,sans-serif" class=" es-m-txt-c">Cordialement,</h6><h6 style="font-family:helvetica,&#39;helvetica neue&#39;,arial,verdana,sans-serif" class=" es-m-txt-c">Le support</h6><h6 style="font-family:helvetica,&#39;helvetica neue&#39;,arial,verdana,sans-serif" class=" es-m-txt-c"><strong>Éco-service</strong></h6></td>
+                       </tr>
+                       
+                      </tbody>
+                     </table></td>
+                   </tr>
+                  </tbody>
+                 </table></td>
+               </tr>
+              </tbody>
+             </table></td>
+           </tr>
+          </tbody>
+         </table><table class="es-content" cellspacing="0" cellpadding="0" align="center"><tbody><tr><td class="esd-stripe" align="center">
+              
+              
+          
+      <table class="es-footer-body" width="600" cellspacing="0" cellpadding="0" align="center">
+          <tbody>
+              <tr>
+                  <td class="esd-structure es-p20t es-p20r es-p20l es-m-p0t" esd-general-paddings-checked="false" align="left" bgcolor="#1c3242" style="background-color:#1c3242">
+              
+                      <table cellspacing="0" cellpadding="0" width="100%">
+                          <tbody>
+                              <tr>
+                                  <td class="esd-container-frame" width="560" align="left">
+              
+                                      <table width="100%" cellspacing="0" cellpadding="0">
+                                          <tbody>
+                                              <tr>
+              <td align="center" class="esd-block-image" style="font-size: 0">
+                  <a target="_blank">
+                      <img src="https://efrwlxp.stripocdn.email/content/guids/CABINET_18a75c870884b71ca782936b8685f9926556f0fb81ea8f82fc8118f20da9bfda/images/logo_eco_service.png" alt="" width="100" class="img-2691">
+                  </a>
+              </td>
+          </tr><tr>
+              <td align="left" class="esd-block-text es-text-1351">
+                  <h6 style="color:#fff;font-size:13px;line-height:150%;font-family:helvetica,&#39;helvetica neue&#39;,arial,verdana,sans-serif" align="center" class=" es-m-txt-c"><strong>25 RUE MARC DONADILLE - 13013 MARSEILLE - ecoservice@gmail.com - 0102030405</strong></h6>
+              </td>
+          </tr>
+                                              
+                                          </tbody>
+                                      </table>
+                                  </td>
+                              </tr>
+                          </tbody>
+                      </table>
+                  </td>
+              </tr>
+          </tbody>
+      </table>
+  </td></tr></tbody></table>
+         
+         
+         
+         </td>
+       </tr>
+      </tbody>
+     </table>
+    </div>
+   
+  </body></html>
